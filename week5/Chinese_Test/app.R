a94d83f9a44e87f466050a2fc992e6f47c0af5df
pccuyao <40812714+pccuyao@users.noreply.github.com>
2018-08-07 21:07:09 +0800

    ADD FILE

---
+++ week5/Chinese_Test/app.R
@@ -0,0 +1,32 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  fluidRow(
+   # Application title
+   titlePanel("R Test"),
+        dataTableOutput("R_Status")
+
+   )
+   )
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$R_Status <- renderDataTable({
+     installed.packages()
+     })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
